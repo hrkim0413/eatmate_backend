ff9ac32ac706d0d8c06effdb52a4e6b4ddfe92a4
hrkim0413 <hrkim041396@gmail.com>
2026-02-09 12:57:58 +0900

    db 정보 수정

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql'); // 또는 mysql2
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'dadtabase',
   user: 'root',
   password: '1234',
-  database: 'eatmate'
+  database: 'kdt'
 });
 
 connection.connect((err) => {
